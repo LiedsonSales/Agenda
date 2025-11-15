4b11531dab1cc2b2c97db81fa899f04605d85c07
Liedson Sales <liedson.r.sales@gmail.com>
2025-11-14 21:51:45 -0300

    subindo página inicial de teste da aplicação no localhost

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const route = require('./routes');
+const path = require('path');
+
+app.use(express.urlencoded({extended: true}));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(route);
+
+app.listen(3000, () => {
+    console.log('Running server in port 3000...');
+    console.log('http://localhost:3000');
+})
