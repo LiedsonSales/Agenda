cced986bf72db70d2cbf54230369757b69c0bd54
Liedson Sales <liedson.r.sales@gmail.com>
2025-11-17 15:17:20 -0300

    adicionado cookie de uma semana

--- server.js
+++ server.js
@@ -9,14 +9,35 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     console.log('Conectado com sucesso!');
 }).catch(e => console.log(e));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
 const route = require('./routes');
 const path = require('path');
 const {middlewareModelo} = require('./src/middlewares/middleware')
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'dhjak shkd',
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        mongoUrl: process.env.CONNECTIONSTRING
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+});
+app.use(sessionOptions);
+app.use(flash());
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.use(middlewareModelo);
 app.use(route);
