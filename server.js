658a15ec38e0df799ea7d48b2c6d822766ca7279
Liedson Sales <liedson.r.sales@gmail.com>
2025-11-17 07:50:07 -0300

    atualizada página de teste | adicionado webpack

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 app.use(express.urlencoded({extended: true}));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.use(route);
 
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './frontend/main.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'), //caminho do arquivo
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
+    devtool: 'source-map'
+}
